a14bf3a11cd0e73f089a2b6fda526cce0c4d3a65
Nanjiang Shu <nanjiang.shu@bils.se>
2016-09-13 23:07:48 +0200

    add install_docker_centos7.sh

---
+++ install_docker_centos7.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+sudo tee /etc/yum.repos.d/docker.repo <<-'EOF'
+[dockerrepo]
+name=Docker Repository
+baseurl=https://yum.dockerproject.org/repo/main/fedora/$releasever/
+enabled=1
+gpgcheck=1
+gpgkey=https://yum.dockerproject.org/gpg
+EOF
+
+sudo yum install -y docker-engine
+sudo systemctl enable docker.service
+sudo systemctl start docker
+sudo usermod -aG docker $USER; sudo systemctl restart docker;
+curl -L https://github.com/docker/compose/releases/download/1.8.0-rc1/docker-compose-`uname -s`-`uname -m` > /tmp/docker-compose; chmod +x /tmp/docker-compose; sudo cp /tmp/docker-compose /usr/local/bin/docker-compose

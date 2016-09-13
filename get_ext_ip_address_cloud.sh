9ef604939f66ec18ba1060af6c8a820524a880ba
Nanjiang Shu <nanjiang.shu@gmail.com>
2016-09-13 22:50:52 +0200

    add description

--- get_ext_ip_address_cloud.sh
+++ get_ext_ip_address_cloud.sh
@@ -1,3 +1,6 @@
 #/bin/bash
 
+# Description: 
+#   get the IP address of the running computer.
+#   This is useful to get the public (floating) IP within a virutal machine    
 dig +short myip.opendns.com @resolver1.opendns.com

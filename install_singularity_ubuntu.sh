f5480e05b163907df876e5ebabe3b91388ac3c2a
nanjiangshu <nanjiang.shu@gmail.com>
2018-02-05 14:15:28 +0100

    add script to install singularity on ubuntu/debian

---
+++ install_singularity_ubuntu.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+VERSION=2.4.2
+wget https://github.com/singularityware/singularity/releases/download/$VERSION/singularity-$VERSION.tar.gz
+tar xvf singularity-$VERSION.tar.gz
+cd singularity-$VERSION
+./configure --prefix=/usr/local
+make
+sudo make install
+
+

75d6a897a52f442c3e7ac93e579bfb57d1519b3f
nanjiangshu <nanjiang.shu@gmail.com>
2019-12-05 21:58:44 +0100

    update docker-compose version

--- install_docker_ubuntu16.sh
+++ install_docker_ubuntu16.sh
@@ -2,5 +2,5 @@
 curl -fsSL https://get.docker.com/ | sh
 
 if [ ! -f   /usr/local/bin/docker-compose ];then
-    curl -L https://github.com/docker/compose/releases/download/1.23.1/docker-compose-`uname -s`-`uname -m` > /tmp/docker-compose; chmod +x /tmp/docker-compose; sudo cp /tmp/docker-compose /usr/local/bin/docker-compose
+    curl -L https://github.com/docker/compose/releases/download/1.25.0/docker-compose-`uname -s`-`uname -m` > /tmp/docker-compose; chmod +x /tmp/docker-compose; sudo cp /tmp/docker-compose /usr/local/bin/docker-compose
 fi

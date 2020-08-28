354a82cc226ce5618d2a20db7cdee00fc76af39b
nanjiangshu <nanjiang.shu@gmail.com>
2020-08-28 15:03:54 +0200

    update format_block_vol_1.sh

--- format_block_vol_1.sh
+++ format_block_vol_1.sh
@@ -1,10 +1,24 @@
 #!/bin/bash
 
 # format the block storage /dev/vdb
+usage="
+$0 DEV MountFolder
+
+e.g.
+
+$0 vdb /media/storage
+"
+dev=$1
+mountpath=$2
+
+if [ "$dev" == "" || "$mountpath" == "" ];then
+    echo "$usage"
+    exit 1
+fi
 
 sudo fdisk /dev/vdb # n then w
-sudo mkfs -t ext4 /dev/vdb1 
-echo "/dev/vdb1  /media/storage ext4 defaults,noatime,_netdev,nofail 0 2" | sudo tee --append /etc/fstab
-sudo mkdir /media/storage
+sudo mkfs -t ext4 /dev/${dev}1 
+echo "/dev/{$dev}1  $mountpath ext4 defaults,noatime,_netdev,nofail 0 2" | sudo tee --append /etc/fstab
+sudo mkdir $mountpath
 sudo mount -a 
 

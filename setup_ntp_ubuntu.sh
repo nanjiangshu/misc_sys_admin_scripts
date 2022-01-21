ac4e1b898b8853ce17c26c1dadb00af6f900df35
nanjiangshu <nanjiang.shu@gmail.com>
2022-01-21 10:29:30 +0100

    fix: add package ntpdate

--- setup_ntp_ubuntu.sh
+++ setup_ntp_ubuntu.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 sudo apt-get install -y ntp
+sudo apt install ntpdate
 echo "ntpdate ntp.ubuntu.com pool.ntp.org" | sudo tee --append /etc/cron.daily/ntpdate
 sudo chmod 755  /etc/cron.daily/ntpdate
 sudo /etc/init.d/ntp stop

05226074f2d1a18b4924ce1a2f2165ded55c1399
nanjiangshu <nanjiang.shu@gmail.com>
2022-02-02 21:49:58 +0100

    feat: add script to install apache on centos8

---
+++ install_apache2_on_centos8.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+sudo yum clean all
+
+sudo yum -y update
+
+sudo yum -y install httpd
+
+sudo yum install firewall-config
+
+sudo firewall-cmd --permanent --add-port=80/tcp
+sudo firewall-cmd --permanent --add-port=443/tcp
+sudo firewall-cmd --reload
+sudo systemctl start httpd
+sudo systemctl enable httpd
+sudo systemctl status httpd

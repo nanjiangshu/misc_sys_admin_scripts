22e727e9496f1c4718c4655416d33baa7fa559b2
nanjiangshu <nanjiang.shu@gmail.com>
2022-02-02 21:50:53 +0100

    feat: update

--- install_apache2_on_centos8.sh
+++ install_apache2_on_centos8.sh
@@ -8,9 +8,10 @@ sudo yum -y install httpd
 
 sudo yum install firewall-config
 
-sudo firewall-cmd --permanent --add-port=80/tcp
-sudo firewall-cmd --permanent --add-port=443/tcp
+sudo firewall-cmd --permanent --add-service=https
+sudo firewall-cmd --permanent --add-service=http
 sudo firewall-cmd --reload
+
 sudo systemctl start httpd
 sudo systemctl enable httpd
 sudo systemctl status httpd

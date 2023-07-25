3971c0d17d8495196dfbf7ca5ba372672f4409bc
Nanjiang Shu <nanjiang.shu@gmail.com>
2023-07-25 14:24:10 +0200

    install also docker compose CLI

--- install_docker_ubuntu16.sh
+++ install_docker_ubuntu16.sh
@@ -2,5 +2,9 @@
 curl -fsSL https://get.docker.com/ | sh
 
 if [ ! -f   /usr/local/bin/docker-compose ];then
-    curl -L https://github.com/docker/compose/releases/download/1.27.4/docker-compose-`uname -s`-`uname -m` > /tmp/docker-compose; chmod +x /tmp/docker-compose; sudo cp /tmp/docker-compose /usr/local/bin/docker-compose
+    curl -L https://github.com/docker/compose/releases/download/2.20.2/docker-compose-`uname -s`-`uname -m` > /tmp/docker-compose; chmod +x /tmp/docker-compose; sudo cp /tmp/docker-compose /usr/local/bin/docker-compose
 fi
+
+
+mkdir -p ~/.docker/cli-plugins/
+curl -SL https://github.com/docker/compose/releases/download/v2.20.0/docker-compose-linux-x86_64 -o ~/.docker/cli-plugins/docker-compose && chmod +x ~/.docker/cli-plugins/docker-compose

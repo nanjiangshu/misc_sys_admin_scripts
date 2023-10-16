3606af3608e6b4b6619751353b19a0201c0fca4d
Nanjiang Shu <nanjiang.shu@gmail.com>
2023-10-16 15:47:48 +0200

    update script to obtain external IP

--- get_ext_ip_address_cloud.sh
+++ get_ext_ip_address_cloud.sh
@@ -3,4 +3,11 @@
 # Description: 
 #   get the IP address of the running computer.
 #   This is useful to get the public (floating) IP within a virutal machine    
-dig +short myip.opendns.com @resolver1.opendns.com
+#
+external_ip=$(dig +short myip.opendns.com @resolver1.opendns.com)
+
+if [ "$external_ip" == "" ];then
+    external_ip=$(curl https://myip.dnsomatic.com/ 2> /dev/null)
+fi
+
+echo "$external_ip"

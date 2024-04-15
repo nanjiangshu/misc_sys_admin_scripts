4f9f00ab0f0994996ff77ead4dda08c6eb49b7f6
Nanjiang Shu <nanjiang.shu@gmail.com>
2024-04-15 16:05:14 +0200

    update script to install docker

--- install_docker_ubuntu16.sh
+++ install_docker_ubuntu16.sh
@@ -1,10 +1,6 @@
 
 curl -fsSL https://get.docker.com/ | sh
 
-if [ ! -f   /usr/local/bin/docker-compose ];then
-    curl -L https://github.com/docker/compose/releases/download/2.20.2/docker-compose-`uname -s`-`uname -m` > /tmp/docker-compose; chmod +x /tmp/docker-compose; sudo cp /tmp/docker-compose /usr/local/bin/docker-compose
-fi
-
 
 mkdir -p ~/.docker/cli-plugins/
-curl -SL https://github.com/docker/compose/releases/download/v2.20.0/docker-compose-linux-x86_64 -o ~/.docker/cli-plugins/docker-compose && chmod +x ~/.docker/cli-plugins/docker-compose
+curl -SL https://github.com/docker/compose/releases/download/v2.24.0/docker-compose-linux-x86_64 -o ~/.docker/cli-plugins/docker-compose && chmod +x ~/.docker/cli-plugins/docker-compose

da09e6ddd833a7207fda53019697538c57d375cc
Nanjiang Shu <nanjiang.shu@gmail.com>
2024-04-25 21:16:38 +0200

    add script to install remote desktop on ubuntu 22

--- install_dep_ubuntu22.sh
+++ install_dep_ubuntu22.sh
@@ -1,5 +1,9 @@
 # install dependencies on ubuntu22 when the after the VM is created
 
+
+sudo apt install x11-apps -y
+sudo apt install firefox -y
+
 bash ./install_docker_ubuntu.sh
 bash ./install_go_on_linux.sh 1.22.2
 bash ./install_et_ubuntu.sh

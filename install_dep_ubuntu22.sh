54e0174364947b9e8cd28d9aa61b36948c06653c
Nanjiang Shu <nanjiang.shu@gmail.com>
2024-04-25 23:02:21 +0200

    update script for ubuntu22

--- install_dep_ubuntu22.sh
+++ install_dep_ubuntu22.sh
@@ -3,6 +3,11 @@
 
 sudo apt install x11-apps -y
 sudo apt install firefox -y
+sudo apt install keychain -y
+sudo apt install jq -y
+sudo apt install samtools -y
+sudo apt install tree -y
+
 
 bash ./install_docker_ubuntu.sh
 bash ./install_go_on_linux.sh 1.22.2

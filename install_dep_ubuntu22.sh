41072e38a4c653b6ed8408d3722a9882af97af73
nanjiangshu <nanjiang.shu@gmail.com>
2024-05-14 11:53:30 +0200

    update script to instsall dependencies on ubuntu22

--- install_dep_ubuntu22.sh
+++ install_dep_ubuntu22.sh
@@ -1,4 +1,6 @@
 # install dependencies on ubuntu22 when the after the VM is created
+rundir=$(dirname $0)
+rundir=$(readlink -f $rundir)
 
 sudo apt install x11-apps -y
 sudo apt install firefox -y
@@ -8,12 +10,9 @@ sudo apt install samtools -y
 sudo apt install tree -y
 sudo apt install imagemagick -y
 
-
-bash ./install_docker_ubuntu.sh
-bash ./install_go_on_linux.sh 1.22.2
-bash ./install_singularity_on_linux.sh
-bash ./install_miniconda_on_linux.sh
-
-bash ./install_et_ubuntu.sh
-
-
+bash ${rundir}/install_docker_ubuntu.sh
+bash ${rundir}/install_go_on_linux.sh 1.22.2
+bash ${rundir}/install_singularity_on_linux.sh
+bash ${rundir}/install_miniconda_on_linux.sh
+bash ${rundir}/install_et_ubuntu.sh
+bash ${rundir}/setup_ntp_ubuntu.sh

6d37846322b665e799f20848d2dfe818811f33fe
nanjiangshu <nanjiang.shu@gmail.com>
2024-05-14 16:12:22 +0200

    set slurm status to idle

--- reload_slurm.sh
+++ reload_slurm.sh
@@ -4,4 +4,7 @@ sudo systemctl stop slurmctld
 sudo systemctl stop slurmd
 sudo systemctl start slurmctld
 sudo systemctl start slurmd
+
+hostname=$(hostname -s)
+sudo scontrol update nodename=$hostname state=idle 
 sinfo -R

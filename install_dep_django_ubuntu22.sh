544e3dcab476f1a1b1c2bbf8c762b59a661795a4
nanjiangshu <nanjiang.shu@gmail.com>
2024-05-14 21:33:34 +0200

    add script

---
+++ install_dep_django_ubuntu22.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+sudo apt -y install python-dev
+
+# installing mod_wsgi
+sudo apt -y install apache2-dev
+sudo apt -y install httpd-wsgi 
+
+
+# for set limitation for maximum connections
+tmpdir=$(mktemp -d /tmp/tmp.XXXXXXXXX) || { echo "Failed to create temp dir" >&2; exit 1; }
+cd $tmpdir
+wget http://dominia.org/djao/limit/mod_limitipconn-0.24.tar.bz2
+tar xavf mod_limitipconn-0.24.tar.bz2
+cd mod_limitipconn-0.24
+sudo make install
+
+cd 
+sudo rm -rf $tmpdir
+
+

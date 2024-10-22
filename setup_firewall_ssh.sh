285152daa56498341627502be2394646eb1a4694
Nanjiang Shu <nanjiang.shu@gmail.com>
2024-10-22 16:00:06 +0200

    enable port 2022 on firewall rules

--- setup_firewall_ssh.sh
+++ setup_firewall_ssh.sh
@@ -1,6 +1,9 @@
 sudo ufw allow from 130.237.0.0/16 to any port 22
 sudo ufw allow from 130.238.0.0/16 to any port 22
 sudo ufw allow from 130.229.169.0/24 to any port 22
+sudo ufw allow from 130.237.0.0/16 to any port 2022/tcp
+sudo ufw allow from 130.238.0.0/16 to any port 2022/tcp
+sudo ufw allow from 130.229.169.0/24 to any port 2022/tcp
 sudo ufw deny 22
 sudo ufw enable
 sudo systemctl enable ufw

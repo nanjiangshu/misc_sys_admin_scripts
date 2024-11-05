244c3b24f43c70d21f09ecf03c634ee1cc5da01c
Nanjiang Shu <nanjiang.shu@gmail.com>
2024-11-05 09:57:52 +0100

    update

--- setup_firewall_ssh.sh
+++ setup_firewall_ssh.sh
@@ -8,6 +8,8 @@ sudo ufw allow from 155.4.221.201 to any port 22
 sudo ufw allow from 155.4.221.201 to any port 2022
 sudo ufw allow from 212.25.149.237 to any port 22
 sudo ufw allow from 212.25.149.237 to any port 2022
+sudo ufw allow from 130.243.159.173/16 to any port 22
+sudo ufw allow from 130.243.159.173/16 to any port 2022
 sudo ufw deny 22
 sudo ufw enable
 sudo systemctl enable ufw

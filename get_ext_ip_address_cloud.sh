8706d63621b11ab702f06b5a4fcf7b44886af8f6
Nanjiang Shu <nanjiang.shu@gmail.com>
2025-05-15 09:31:43 +0200

    update script for getting external IPs of the host

--- get_ext_ip_address_cloud.sh
+++ get_ext_ip_address_cloud.sh
@@ -4,10 +4,10 @@
 #   get the IP address of the running computer.
 #   This is useful to get the public (floating) IP within a virutal machine    
 #
-external_ip=$(dig +short myip.opendns.com @resolver1.opendns.com)
+external_ip=$(curl https://myip.dnsomatic.com/ 2> /dev/null)
 
 if [ "$external_ip" == "" ];then
-    external_ip=$(curl https://myip.dnsomatic.com/ 2> /dev/null)
+    external_ip=$(dig +short myip.opendns.com @resolver1.opendns.com 2> /dev/null)
 fi
 
 echo "$external_ip"

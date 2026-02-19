4c8a03496671084c09faa9f8c05530684356ea32
Nanjiang Shu <nanjiang.shu@gmail.com>
2026-02-19 22:16:00 +0100

    update script

--- install_helm_on_ubuntu.sh
+++ install_helm_on_ubuntu.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 set -euo pipefail
+# This script is used to install helm on ubuntu
+tempDir=$(mktemp -d)
 
-# 1. Import the Helm GPG key
-curl https://baltocdn.com/helm/signing.asc | gpg --dearmor | sudo tee /usr/share/keyrings/helm.gpg > /dev/null
+pushd $tempDir
 
-# 2. Install the apt-transport-https package (if not already installed)
-sudo apt-get install apt-transport-https --yes
+curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3
+chmod 700 get_helm.sh
 
-# 3. Add the Helm repository to your sources list
-echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/helm.gpg] https://baltocdn.com/helm/stable/debian/ all main" | sudo tee /etc/apt/sources.list.d/helm-stable-debian.list
+./get_helm.sh
 
-# 4. Update and install
-sudo apt-get update
-sudo apt-get install helm
+popd
+
+rm -rf $tempDir

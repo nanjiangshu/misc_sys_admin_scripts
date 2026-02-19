8f08eb82acea304587885aa3cdf5060f7ad23cc0
Nanjiang Shu <nanjiang.shu@gmail.com>
2026-02-19 13:53:21 +0100

    feat: update script to install docker and compose CLI on ubuntu

--- install_docker_ubuntu.sh
+++ install_docker_ubuntu.sh
@@ -1,9 +1,21 @@
 #!/bin/bash
+set -euo pipefail
+
+# This script is used to install docker and docker compose CLI on ubuntu
 
 curl -fsSL https://get.docker.com/ | sh
 
 sudo usermod -aG docker $USER
 
 # install docker compose CLI
+defaultVersion="v5.0.2"
+version=""
+latestVersion=$(curl -v  https://github.com/docker/compose/releases/latest 2>&1 | grep location | awk -F"/" '{print $NF}')
+if [ "$latestVersion" = "" ]; then
+  version=$defaultVersion
+else
+  version=$latestVersion
+fi
+
 mkdir -p ~/.docker/cli-plugins/
-curl -SL https://github.com/docker/compose/releases/download/v2.26.0/docker-compose-linux-x86_64 -o ~/.docker/cli-plugins/docker-compose && chmod +x ~/.docker/cli-plugins/docker-compose
+curl -SL https://github.com/docker/compose/releases/download/$version/docker-compose-linux-x86_64 -o ~/.docker/cli-plugins/docker-compose && chmod +x ~/.docker/cli-plugins/docker-compose

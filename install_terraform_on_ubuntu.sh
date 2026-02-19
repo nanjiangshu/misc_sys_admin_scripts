4fed71d37d0d6f87091b71475d680a1f7d20e6c3
Nanjiang Shu <nanjiang.shu@gmail.com>
2026-02-19 21:40:14 +0100

    add script to install  terrafrom on ubuntu

---
+++ install_terraform_on_ubuntu.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+set -euo pipefail
+
+# This script is used to install terraform on ubuntu
+
+sudo apt update
+sudo apt install -i git unzip
+
+# Clone the repository
+git clone --depth=1 https://github.com/tfutils/tfenv.git ~/.tfenv
+
+# Add tfenv to your PATH so you can run it from anywhere
+echo 'export PATH="$HOME/.tfenv/bin:$PATH"' >> ~/.bashrc
+
+# Reload your shell configuration
+source ~/.bashrc
+
+
+tfenv --version
+
+tfenv install latest
